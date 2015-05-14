0bb8007c08f6d04bf17fe5e6a46f1155d8c2879b
yannael <yannael@gmail.com>
2015-05-14 12:08:13 +0200

    Align folder structure to ShinyApp version

--- explorer/global.R
+++
@@ -1,9 +0,0 @@
-#This script loads the data from TED database (CSV exported to SQLite database - see preprocessing.R), 
-#and stores a subset of in session data 
-
-authority_countries<-c("All",read.table("data/authority_countries.txt",stringsAsFactors=F,col.names=F)[,1])
-operator_countries<-c("All",read.table("data/operator_countries.txt",stringsAsFactors=F,col.names=F)[,1])
-
-dataNamesFields<-read.table("data/nameFields.txt",stringsAsFactors=F)
-nameFields<-dataNamesFields[,1]
-names(nameFields)<-dataNamesFields[,2]
