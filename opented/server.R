8a6ba464972b0754e52c42ca8ace5b4b16e07711
Yannael <yannael@gmail.com>
2015-05-31 18:19:12 +0200

    Export CSV/GEXF

--- opented/server.R
+++ opented/server.R
@@ -3,11 +3,15 @@ library(RMySQL)
 library(DT)
 library(RCurl)
 library(networkD3)
+library(rgexf)
+
+source("myDT.R")
 
 shinyServer(function(input, output,session) {
   
   sessionData <- reactiveValues()
   sessionData$nbRowsErrorMessage<-""
+  
   
   output$nbRowsErrorMessage<-renderText({
     sessionData$nbRowsErrorMessage
@@ -19,25 +23,12 @@ shinyServer(function(input, output,session) {
       fields<-'count(1)'
     else fields<-paste(nameFields[1:13],collapse=",")
     
-    country_select_authority<-""
-    country_select_operator<-""
-    CPV<-""
-    #if ((CPVFrom!="") & (CPVTo != "")) CPV<-paste0(" and contract_cpv_code>=",CPVFrom," and contract_cpv_code<=",CPVTo)
-    value<-""
-    #if ((valueFrom!="") & (valueTo != "")) value<-paste0(" and contract_contract_value_cost_eur>=",valueFrom," and contract_contract_value_cost_eur<=",valueTo)
-    nbOffers<-""
-    #if ((nbOffersFrom!="") & (nbOffersTo != "")) nbOffers<-paste0(" and contract_offers_received_num>=",nbOffersFrom," and contract_offers_received_num<=",nbOffersTo)
     con <- dbConnect(RSQLite::SQLite(), "data/TED_award_notices.db")
     rs<-dbSendQuery(con,paste0("select ", fields," 
                                    from contracts 
                                    where 
                                    document_oj_date>='",dateRange[1],"' ", 
                                "and document_oj_date<='",dateRange[2],"' ",
-                               country_select_authority,
-                               country_select_operator,
-                               CPV,
-                               value,
-                               nbOffers,
                                " ORDER BY document_oj_date ASC"
     ))
     results = fetch(rs, n=-1)
@@ -79,161 +70,6 @@ shinyServer(function(input, output,session) {
     })
   })
   
-  shinyFun = function(name) getFromNamespace(name, 'shiny')
-  
-  require(lubridate)
-  
-  dataTablesFilter = function(data, params) {
-    n = nrow(data)
-    q = params
-    ci = q$search[['caseInsensitive']] == 'true'
-    
-    # global searching
-    i = seq_len(n)
-    # for some reason, q$search might be NULL, leading to error `if (logical(0))`
-    if (isTRUE(q$search[['value']] != '')) {
-      i0 = apply(data, 2, function(x) {
-        grep2(q$search[['value']], as.character(x),
-              fixed = q$search[['regex']] == 'false', ignore.case = ci)
-      })
-      i = intersect(i, unique(unlist(i0)))
-    }
-    
-    # search by columns
-    if (length(i)) for (j in names(q$columns)) {
-      col = q$columns[[j]]
-      # if the j-th column is not searchable or the search string is "", skip it
-      if (col[['searchable']] != 'true') next
-      if ((k <- col[['search']][['value']]) == '') next
-      j = as.integer(j)
-      dj = data[, j + 1]
-      ij = if (is.numeric(dj) || is.Date(dj)) {
-        r = commaToRange(k)
-        if (length(r) != 2)
-          stop('The range of a numeric / date / time column must be of length 2')
-        if (is.Date(dj)) {
-          # r is milliseconds
-          r = as.POSIXct(r / 1000, origin = '1970-01-01')
-          if (inherits(dj, 'Date')) r = as.Date(r)
-        }
-        which(dj >= r[1] & dj <= r[2])
-      } else if (is.factor(dj)) {
-        which(dj %in% jsonlite::fromJSON(k))
-      } else {
-        grep2(k, as.character(dj), fixed = col[['search']][['regex']] == 'false',
-              ignore.case = ci)
-      }
-      i = intersect(ij, i)
-      if (length(i) == 0) break
-    }
-    if (length(i) != n) data = data[i, , drop = FALSE]
-    
-    # sorting
-    oList = list()
-    for (ord in q$order) {
-      k = ord[['column']]  # which column to sort
-      d = ord[['dir']]     # direction asc/desc
-      if (q$columns[[k]][['orderable']] != 'true') next
-      col = data[, as.integer(k) + 1]
-      oList[[length(oList) + 1]] = (if (d == 'asc') identity else `-`)(
-        if (is.numeric(col)) col else xtfrm(col)
-      )
-    }
-    if (length(oList)) {
-      i = do.call(order, oList)
-      data = data[i, , drop = FALSE]
-    }
-    # paging
-    if (q$length != '-1') {
-      i = seq(as.integer(q$start) + 1L, length.out = as.integer(q$length))
-      i = i[i <= nrow(data)]
-      fdata = data[i, , drop = FALSE]  # filtered data
-    } else fdata = data
-    
-    fdata = unname(as.matrix(fdata))
-    if (is.character(fdata) && q$escape != 'false') {
-      if (q$escape == 'true') fdata = htmlEscape(fdata) else {
-        k = as.integer(strsplit(q$escape, ',')[[1]])
-        # use seq_len() in case escape = negative indices, e.g. c(-1, -5)
-        for (j in seq_len(ncol(fdata))[k]) fdata[, j] = htmlEscape(fdata[, j])
-      }
-    }
-    
-    sessionData$currentData<-data
-    
-    list(
-      draw = as.integer(q$draw),
-      recordsTotal = n,
-      recordsFiltered = nrow(data),
-      data = fdata
-    )
-  }
-  
-  # when both ignore.case and fixed are TRUE, we use grep(ignore.case = FALSE,
-  # fixed = TRUE) to do lower-case matching of pattern on x
-  grep2 = function(pattern, x, ignore.case = FALSE, fixed = FALSE, ...) {
-    if (fixed && ignore.case) {
-      pattern = tolower(pattern)
-      x = tolower(x)
-      ignore.case = FALSE
-    }
-    # when the user types in the search box, the regular expression may not be
-    # complete before it is sent to the server, in which case we do not search
-    if (!fixed && inherits(try(grep(pattern, ''), silent = TRUE), 'try-error'))
-      return(seq_along(x))
-    grep(pattern, x, ignore.case = ignore.case, fixed = fixed, ...)
-  }
-  
-  # convert a string of the form "lower,upper" to c(lower, upper)
-  commaToRange = function(string) {
-    if (!grepl(',', string)) return()
-    r = strsplit(string, ',')[[1]]
-    if (length(r) > 2) return()
-    if (length(r) == 1) r = c(r, '')  # lower,
-    r = as.numeric(r)
-    if (is.na(r[1])) r[1] = -Inf
-    if (is.na(r[2])) r[2] = Inf
-    r
-  }
-  
-  mydataTableAjax = function(session, data, rownames, filter = dataTablesFilter) {
-    oop = options(stringsAsFactors = FALSE); on.exit(options(oop), add = TRUE)
-    
-    # abuse tempfile() to obtain a random id unique to this R session
-    id = basename(tempfile(''))
-    
-    # deal with row names: rownames = TRUE or missing, use rownames(data)
-    rn = if (missing(rownames) || isTRUE(rownames)) base::rownames(data) else {
-      if (is.character(rownames)) rownames  # use custom row names
-    }
-    data = as.data.frame(data)  # think dplyr
-    if (length(rn)) data = cbind(' ' = rn, data)
-    
-    URLdecode = shinyFun('URLdecode')
-    toJSON = shinyFun('toJSON')
-    httpResponse = shinyFun('httpResponse')
-    
-    filterFun = function(data, req) {
-      # DataTables requests were sent via POST
-      params = URLdecode(rawToChar(req$rook.input$read()))
-      Encoding(params) = 'UTF-8'
-      # use system native encoding if possible (again, this grep(fixed = TRUE) bug
-      # https://bugs.r-project.org/bugzilla3/show_bug.cgi?id=16264)
-      params2 = iconv(params, 'UTF-8', '')
-      if (!is.na(params2)) params = params2 else warning(
-        'Some DataTables parameters contain multibyte characters ',
-        'that do not work in current locale.'
-      )
-      params = shiny::parseQueryString(params, nested = TRUE)
-      
-      res = tryCatch(filter(data, params), error = function(e) {
-        list(error = as.character(e))
-      })
-      httpResponse(200, 'application/json', enc2utf8(toJSON(res)))
-    }
-    
-    session$registerDataObj(id, data, filterFun)
-  }
   
   #Returns the dataset in the form of a table
   output$awardTable<-DT::renderDataTable({
@@ -241,20 +77,14 @@ shinyServer(function(input, output,session) {
     data[,3]<-as.factor(data[,3])
     data[,5]<-as.factor(data[,5])
     if (!is.null(dim(data))) {
-      action = mydataTableAjax(session, data)
+      action = mydataTableAjax(session, data,sessionData=sessionData)
       widget = datatable(data, 
                          server = TRUE, 
-                         #extensions = c('Scroller'),
                          escape=F,
                          filter = 'top',
                          options = list(
-                           #dom= 'plrti',
-                           dom= 'itlp',
-                           #scrollX = T,
+                           dom= 'itlpi',
                            ajax = list(url = action),
-                           #deferRender = TRUE,
-                           #scrollY = 500,
-                           #scrollCollapse = T,
                            lengthMenu = list(c(10, 100, 1000, 10000), c('10', '100','1000','10000')),pageLength = 10,
                            autoWidth = F,
                            #                                              list(
@@ -269,6 +99,41 @@ shinyServer(function(input, output,session) {
                            columnDefs = list(
                              list(visible=F,targets=c(0)),
                              list(width=c(150),targets=c(4,6)),
+                             list(className="dt-right",targets="_all")
+                           )
+                         )
+      )
+      widget
+    }
+  })
+  
+  #Returns the dataset in the form of a table
+  output$CPVTable<-DT::renderDataTable({
+    data<-CPVTable
+    if (!is.null(dim(data))) {
+      action = dataTableAjax(session, data)
+      widget = datatable(data, 
+                         server = TRUE, 
+                         escape=F,
+                         filter = 'top',
+                         options = list(
+                           dom= 'tlpi',
+                           ajax = list(url = action),
+                           lengthMenu = list(c(10, 100, -1), c('10', '100','All')),pageLength = 10,
+                           autoWidth = T,
+                           #                                              list(
+                           #                                                targets = c(3,5,6,8,9),
+                           #                                                render = JS(
+                           #                                                  "function(data, type, row, meta) {",
+                           #                                                  "return type === 'display' && data.length > 50 ?",
+                           #                                                  "'<span title=\"' + data + '\">' + data.substr(0, 50) + '...</span>' : data;",
+                           #                                                  "}")
+                           #                                              )
+                           #                            )
+                           columnDefs = list(
+                             list(visible=F,targets=c(0)),
+                             list(width=c(70),targets=c(1)),
+                             list(width=c(300),targets=c(2)),
                              list(className="dt-right",targets="_all")
                            )
                          )
@@ -312,12 +177,77 @@ shinyServer(function(input, output,session) {
     
   })
   
-  output$downloadAwards <- downloadHandler(
+  createGephiFiles<-function() {
+
+    write.csv(sessionData$currentData[,-1], file="selection.csv", row.names=F)
+    
+    data<-sessionData$data[sessionData$currentData[,1],]
+    
+    i.remove<-which(is.na(data[,11]))
+    if (length(i.remove)>0) data<-data[-i.remove,]
+    
+    acquired<-tapply(as.numeric(data[,11]),data[,9],length)
+    awarded<-tapply(as.numeric(data[,11]),data[,6],length)
+    avgcompauth<-tapply(as.numeric(data[,11]),data[,6],mean)
+    avgcompcont<-tapply(as.numeric(data[,11]),data[,9],mean)
+    
+    avgcompauthcat<-floor(avgcompauth)
+    avgcompauthcat[which(avgcompauthcat>6)]<-"6+"
+    auth<-cbind(sort(unique(data[,6])),0,awarded,avgcompauth,avgcompauthcat,'true','false')
+    
+    avgcompcontcat<-floor(avgcompcont)
+    avgcompcontcat[which(avgcompcontcat>6)]<-"6+"
+    cont<-cbind(sort(unique(data[,9])),acquired,0,avgcompcont,avgcompcontcat,'false','true')
+    
+    nodes<-rbind(auth,cont)
+    colnames(nodes)<-c("Id","Contracts acquired","Contracts awarded","Average competition","Average competition (Cat)","Authority","Company")
+    
+    edges<-unique(data[,c(6,9)])
+    colnames(edges)<-c("Source","Target")
+    
+    write.csv(nodes, 'nodes.csv', row.names=F,quote=F)
+    write.csv(edges, 'edges.csv', row.names=F,quote=F)
+    
+    nodesID<-data.frame(ID=1:nrow(nodes),name=nodes[,1])
+    edges[,1]<-match(edges[,1],nodes[,1])
+    edges[,2]<-match(edges[,2],nodes[,1])
+    
+    nodes[nodes[,5]=="6+",5]<-7
+    nodescol<-as.numeric(nodes[,5])+1
+    
+    colors<-matrix(
+      c(255,245,235,1,
+        254,230,206,1,
+        253,208,162,1,
+        253,174,107,1,
+        253,141,60,1,
+        241,105,19,1,
+        217,72,1,1,
+        140,45,4,1),8,4,byrow=T)
+    
+    nodecolors<- data.frame(r = colors[nodescol,1],
+                            g = colors[nodescol,2],
+                            b = colors[nodescol,3],
+                            a = colors[nodescol,4])
+    
+    graph <- write.gexf(nodes=nodesID,
+                        edges=edges,
+                        defaultedgetype='directed',
+                        nodesVizAtt=list(
+                          color=nodecolors,
+                          size=10*(as.numeric(nodes[,2])+as.numeric(nodes[,3]))+100
+                        ),
+                        output="networkGephi.gexf")
+    
+  }
+  
+  output$downloadSelection <- downloadHandler(
     filename = function() {
-      paste('data-', Sys.Date(), '.csv', sep='')
+      paste('data-gephi-', Sys.Date(), '.zip', sep='')
     },
     content = function(con) {
-      write.csv(sessionData$currentData, con, row.names=F)
+      createGephiFiles()
+      zip(con,c('nodes.csv','edges.csv','networkGephi.gexf','selection.csv'))
     }
   )
   
