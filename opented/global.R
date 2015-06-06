b1295cf5a6ad8fda2fb5badbb680e95483d51f14
robertomigue <yann.ael@gmail.com>
2015-06-06 18:49:52 +0200

    Added info to CPV table

--- opented/global.R
+++ opented/global.R
@@ -5,5 +5,6 @@ dataNamesFields<-read.table("data/nameFields.txt",stringsAsFactors=F)
 nameFields<-dataNamesFields[,1]
 names(nameFields)<-dataNamesFields[,2]
 
-CPVTable<-read.table("data/CPVTable.txt",stringsAsFactors=F)
-colnames(CPVTable)<-c("CPV plain code","Category content")
+CPVTable<-read.table("data/CPVTable.txt",stringsAsFactors=F,colClasses=c("character"))
+colnames(CPVTable)<-c("Original code","CPV plain code","NUM-digits", "Real Code", "Category content")
+levelsCPV<-levels(as.factor(CPVTable[,2]))
