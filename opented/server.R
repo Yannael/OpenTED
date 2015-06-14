cbe6d20bc7cf70e0c15c39430ec78c0a08d7d0eb
Yann-Ael Le Borgne <yannael@gmail.com>
2015-06-14 22:29:47 +0200

    Commit issue

--- opented/server.R
+++ opented/server.R
@@ -137,60 +137,68 @@ shinyServer(function(input, output,session) {
     }
   })
   
+  extractURL<-function(link) {
+    strsplit(link,"'")[[1]][2]
+  }
+  
+  
   output$sankey<-renderSankeyNetwork({
     data<-sessionData$data[sessionData$currentData[,1],]
     sessionData$nbContracts<-nrow(data)
     valueToSelect<-'Contract value (€) - Exact'
-    if (nrow(data)>1000) 
-      sessionData$nbRowsErrorMessage<-paste0("Number of selected contracts is ",nrow(data), ", the maximum is 1000. Please refine selection.")
-    else {
-      data<-data[which(data[,valueToSelect]>0),]
-      sessionData$nbContractsMore0<-nrow(data)
-      
-      i.NA<-which(data[,"Contracting authority SLUG"]=="")
-      if (length(i.NA)>0) data[i.NA,"Contracting authority SLUG"]<-"NA"
-      i.NA<-which(data[,"Contractor SLUG"]=="")
-      if (length(i.NA)>0) data[i.NA,"Contractor SLUG"]<-"NA"
-      
-      data<-droplevels(data)
-      pairscontracts<-paste(data$'Contracting authority SLUG',data$'Contractor SLUG',sep="_")
-      
-      sumpairs<-tapply(data[,valueToSelect],pairscontracts,sum)
-      
-      nNodes <- sort(unique(c(as.character(data[,'Contracting authority SLUG']),as.character(data[,'Contractor SLUG']))))
-      
-      nNodes <- unique(unlist(strsplit(names(sumpairs[sort(sumpairs,ind=T,dec=T)$ix]),"_")))
-      
-      nodes <- data.frame(id = 1:length(nNodes),
-                          names = nNodes)
-      
-      relations <- (data[,c('Contracting authority SLUG','Contractor SLUG')])
-      relations[,1]<-match(relations[,1],nodes$names)                                 
-      relations[,2]<-match(relations[,2],nodes$names)
-      names(relations) <- c("source", "target")
-      graphtable<-cbind(relations-1,value=data[,valueToSelect])
-      graphtable<-graphtable[order(-graphtable[,3],graphtable[,1],graphtable[,2]),]
-      
-      nodes[,2]<-as.character(nodes[,2])
-      i.match<-match(nodes[,2],data[,'Contracting authority SLUG'])
-      nodes[which(!is.na(i.match)),2]<-data[i.match[!is.na(i.match)],'Contracting authority name']
-      i.match<-match(nodes[,2],data[,'Contractor SLUG'])
-      nodes[which(!is.na(i.match)),2]<-data[i.match[!is.na(i.match)],'Contractor name']
-      
-      for (i in 1:nrow(nodes)) {
-        if (nchar(nodes[i,2])>65) nodes[i,2]<-paste0(substr(nodes[i,2],1,65),"...")
-      }
-      
-      sessionData$heightSankey<-nrow(relations)*25
-      sessionData$nbAuthority<-length(unique(as.character(data[,'Contracting authority SLUG'])))
-      sessionData$nbContractors<-length(unique(as.character(data[,'Contractor SLUG'])))
-      
-      sankeyNetwork(Links = graphtable[,1:3], Nodes = nodes,
-                    Source = "source", Target = "target",
-                    Value = "value", NodeID = "names",
-                    width = '100%', fontsize = 15, nodeWidth = 30,
-                    nodePadding = 15,height=sessionData$heightSankey)
+    data<-data[which(data[,valueToSelect]>0),]
+    sessionData$nbContractsMore0<-nrow(data)
+    sessionData$totalValueContracts<-sum(data[,valueToSelect])
+    sessionData$nbAuthority<-length(unique(as.character(data[,'Contracting authority SLUG'])))
+    sessionData$nbContractors<-length(unique(as.character(data[,'Contractor SLUG'])))
+    
+    validate(
+      need(nrow(data)<1000, paste0("Number of contracts with value>0 is ",nrow(data), ", the maximum is 1000. Please refine selection."))
+    )
+    
+    i.NA<-which(data[,"Contracting authority SLUG"]=="")
+    if (length(i.NA)>0) data[i.NA,"Contracting authority SLUG"]<-"NA"
+    i.NA<-which(data[,"Contractor SLUG"]=="")
+    if (length(i.NA)>0) data[i.NA,"Contractor SLUG"]<-"NA"
+    
+    data<-droplevels(data)
+    pairscontracts<-paste(data$'Contracting authority SLUG',data$'Contractor SLUG',sep="_")
+    
+    sumpairs<-tapply(data[,valueToSelect],pairscontracts,sum)
+    
+    nNodes <- sort(unique(c(as.character(data[,'Contracting authority SLUG']),as.character(data[,'Contractor SLUG']))))
+    
+    nNodes <- unique(unlist(strsplit(names(sumpairs[sort(sumpairs,ind=T,dec=T)$ix]),"_")))
+    
+    nodes <- data.frame(id = 1:length(nNodes),
+                        names = nNodes)
+    
+    relations <- (data[,c('Contracting authority SLUG','Contractor SLUG')])
+    relations[,1]<-match(relations[,1],nodes$names)                                 
+    relations[,2]<-match(relations[,2],nodes$names)
+    names(relations) <- c("source", "target")
+    links<-sapply(data[,2],extractURL)
+    graphtable<-cbind(relations-1,value=data[,valueToSelect],link=links)
+    
+    graphtable<-graphtable[order(-graphtable[,3],graphtable[,1],graphtable[,2]),]
+    
+    nodes[,2]<-as.character(nodes[,2])
+    i.match<-match(nodes[,2],data[,'Contracting authority SLUG'])
+    nodes[which(!is.na(i.match)),2]<-data[i.match[!is.na(i.match)],'Contracting authority name']
+    i.match<-match(nodes[,2],data[,'Contractor SLUG'])
+    nodes[which(!is.na(i.match)),2]<-data[i.match[!is.na(i.match)],'Contractor name']
+    
+    for (i in 1:nrow(nodes)) {
+      if (nchar(nodes[i,2])>65) nodes[i,2]<-paste0(substr(nodes[i,2],1,65),"...")
     }
+    
+    sessionData$heightSankey<-nrow(relations)*25
+    
+    sankeyNetwork(Links = graphtable[,1:4], Nodes = nodes,
+                  Source = "source", Target = "target",
+                  Value = "value", NodeID = "names", 
+                  width = '100%', fontSize = 15, nodeWidth = 30,
+                  nodePadding = 15,height=sessionData$heightSankey)
   })
   
   output$sankeyUI<-renderUI({
@@ -199,18 +207,20 @@ shinyServer(function(input, output,session) {
         strong("Summary"),p(),
         column(3,
                fluidRow(
-                 "Total number of contracts",br(),
-                 "Number of contracts (values>0)",br(),
                  "Number of contracting authority:",br(),
-                 "Number of contractors",br()
+                 "Number of contractors:",br(),
+                 "Total number of contracts:",br(),
+                 "Number of contracts (values>0):",br(),
+                 "Total value of contracts (€)",br()
                )
         ),
         column(2,
                fluidRow(
+                 sessionData$nbAuthority,br(),
+                 sessionData$nbContractors,br(),
                  sessionData$nbContracts,br(),
                  sessionData$nbContractsMore0,br(),
-                 sessionData$nbAuthority,br(),
-                 sessionData$nbContractors,br()
+                 sessionData$totalValueContracts,br()
                )
         )
       )
