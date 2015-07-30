9efb94c65e74f785a725968b6c91101e1aa0bc97
yannael <yannael@gmail.com>
2015-07-30 21:43:46 +0200

    Only retrieves first 100000 records

--- opented/global.R
+++ opented/global.R
@@ -8,25 +8,15 @@ levelsCPV<-levels(as.factor(CPVTable[,2]))
 loadData<-function(db,tablename,sql) {
   if (sql!="") sql<-paste0("where ",sql)
   condb<-dbConnect(RSQLite::SQLite(), paste0(db,".db"))
-  data<-dbGetQuery(condb,paste0("select * from ",tablename," ",sql," limit 1000"))
+  nbrows<-dbGetQuery(condb,paste0("select count(*) from ",tablename," ",sql))
+  limit<-""
+  nbRowsErrorMessage<-""
+  if (nbrows>100000) {
+    limit<-" limit 100000"
+    nbRowsErrorMessage<-paste0("Warning: Your query returns ",nbrows," records The table below only includes the first 100000.")
+  }
+  data<-dbGetQuery(condb,paste0("select * from ",tablename," ",sql,limit))
   dbDisconnect(condb)
-  data
+  list(data=data,nbRowsErrorMessage=nbRowsErrorMessage)
 }
 
-getWidgetTable<-function(data,session) {
-  action <- dataTableAjax(session, data,rownames=F)
-  datatable(data, 
-            selection = 'none',
-            server=T,
-            rownames=F,
-            escape=F,
-            options = list(
-              dom= 'C<"clear">litp',
-              lengthMenu = list(c(10, 100, 1000, 10000), c('10', '100','1000','10000')),pageLength = 10,
-              ajax = list(url = action),
-              columnDefs = list(
-                list(className="dt-right",targets="_all")
-              )
-            )
-  )
-}
