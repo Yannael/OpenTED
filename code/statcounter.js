1ac85ba265d7e69243060baf868fe6b0658b1112
Yann-Aël Le Borgne <yannael@gmail.com>
2016-07-01 06:30:54 +0200

    Delete statcounter.js

--- code/statcounter.js
+++
@@ -1,9 +0,0 @@
-var sc_project=10438955; 
-var sc_invisible=1; 
-var sc_security="81375f2e"; 
-var scJsHost = (("https:" == document.location.protocol) ?
-"https://secure." : "http://www.");
-document.write("<sc"+"ript type='text/javascript' src='" +
-scJsHost+
-"statcounter.com/counter/counter.js'></"+"script>");
-
